b31b277de8a759ff87b448dc8b8916db63cad7d3
Kirill Artimenia <kirill.artimenya@mail.ru>
2020-09-22 13:14:41 +0300

    Updates

---
+++ precache-manifest.78d9c9b483cd9cc0649c673580fa09cd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4a75f4497abb0dfc145b6e85a65d8fe",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9fc0a4fdaf8b2da476ba",
+    "url": "/portfolio/static/css/2.f76e3c0a.chunk.css"
+  },
+  {
+    "revision": "d75655d80db6948c6693",
+    "url": "/portfolio/static/css/main.c226e14d.chunk.css"
+  },
+  {
+    "revision": "9fc0a4fdaf8b2da476ba",
+    "url": "/portfolio/static/js/2.0bd4cc2b.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio/static/js/2.0bd4cc2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d75655d80db6948c6693",
+    "url": "/portfolio/static/js/main.5d6cbdf7.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "4ae95c217573cfbe8f2061fed7e971ae",
+    "url": "/portfolio/static/media/avatar.4ae95c21.png"
+  },
+  {
+    "revision": "0047728b034fbc3d6b2ff059e005f0ce",
+    "url": "/portfolio/static/media/project-1.0047728b.jpg"
+  },
+  {
+    "revision": "f3af745ca4f085d95c9c757a11f4be6f",
+    "url": "/portfolio/static/media/project-2.f3af745c.jpg"
+  },
+  {
+    "revision": "8b5518ddfcadf3cd50ddcd3e6265cae3",
+    "url": "/portfolio/static/media/project-3.8b5518dd.jpg"
+  }
+]);
